d27efbcafd2c0f990cad9bfcaf2cc87865d6596f
umarhasan <umarf4929@gmail.com>
2024-02-23 22:32:51 +0500

    change

--- app/Http/Controllers/WeeklyController.php
+++ app/Http/Controllers/WeeklyController.php
@@ -29,53 +29,50 @@ class WeeklyController extends Controller
     }
 
     public function store(Request $request, $id)
-{
-    
-    $validator = Validator::make($request->all(), [
-        'type.*' => 'required',
-        'file.*' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Adjust the file validation rules as needed
-    ]);
+    {
+        $validator = Validator::make($request->all(), [
+            'type.*' => 'required',
+            'file.*' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Adjust the file validation rules as needed
+        ]);
 
-    if ($validator->fails()) {
-        return redirect()->back()->withErrors($validator)->withInput();
-    }
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
 
-
-    $coverImage = $request->file('video');
-    $coverImageName = Str::uuid() . '.' . $coverImage->getClientOriginalExtension();
-    $videoPath = $coverImage->storeAs('uploads', $coverImageName, 'public');
-        
-    $weekly =new Weekly();
-    $weekly->dog_id = $id;
-    $weekly->title = $request->title;
-    $weekly->description = $request->description;
-    $weekly->video = $videoPath;
-    $weekly->save();
-
-    // Initialize an array to store all file paths
-    $filePaths = [];
-
-    // Handle file upload for each file
-    foreach ($request->file('file') as $key => $file) {
-
-        
         $coverImage = $request->file('video');
-        $fileName = Str::uuid() . '.' . $coverImage->getClientOriginalExtension();
-        $filePaths[] = $coverImage->storeAs('uploads', $fileName, 'public');
-    }
-    // Save data along with file paths in the database
-    foreach ($filePaths as $key => $filePath) {
-        $data = [
-            'weekly_id' => $id,
-            'type' => $request->input('type')[$key], // Access type using the key
-            'image' => $filePath, // Storing file path
-            // Add other fields if needed
-        ];
+        $coverImageName = Str::uuid() . '.' . $coverImage->getClientOriginalExtension();
+        $videoPath = $coverImage->storeAs('uploads', $coverImageName, 'public');
+            
+        $weekly =new Weekly();
+        $weekly->dog_id = $id;
+        $weekly->title = $request->title;
+        $weekly->description = $request->description;
+        $weekly->video = $videoPath;
+        $weekly->save();
+        // Initialize an array to store all file paths
+        $filePaths = [];
+        // Handle file upload for each file
+        foreach ($request->file('file') as $key => $file) {
+            $coverImage = $request->file('video');
+            $fileName = Str::uuid() . '.' . $coverImage->getClientOriginalExtension();
+            $filePaths[] = $coverImage->storeAs('uploads', $fileName, 'public');
+        }
+        // Save data along with file paths in the database
+        foreach ($filePaths as $key => $filePath) {
+            $data = [
+                'weekly_id' => $weekly->id,
+                'type' => $request->input('type')[$key], // Access type using the key
+                'image' => $filePath, // Storing file path
+            ];
 
-        // Save to database
-        WeeklyImage::create($data);
+            // Save to database
+            WeeklyImage::create($data);
+        }
+
+        return redirect()->back()->withSuccess('Data stored successfully.');
     }
 
-    return redirect()->back()->withSuccess('Data stored successfully.');
-}
+    public function edit($id){
+        $weekly = Weekly::with('weekly_image')->find($id);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -68,10 +68,11 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], func
 //    Route::get('/dog_weekly/{id}', [DogProfileController::class, 'weekly'])->name('/dog_weekly/{id}');
     Route::get('/weekly/{id}', [WeeklyController::class, 'index'])->name('weekly.index');
     Route::get('/weekly/create/{id}', [WeeklyController::class, 'create'])->name('weekly.create');
-    Route::get('/weekly/edit/{id}', [WeeklyController::class, 'edit'])->name('weekly.edit');
-    Route::post('/weekly/delete/{id}', [WeeklyController::class, 'destroy'])->name('weekly.destroy');
     Route::put('/weekly/store/{id}', [WeeklyController::class, 'store'])->name('weekly.store');
-
+    Route::get('/weekly/edit/{id}', [WeeklyController::class, 'edit'])->name('weekly.edit');
+    Route::get('/weekly/update/{id}', [WeeklyController::class, 'update'])->name('weekly.update');
+    Route::post('/weekly/delete/{id}', [WeeklyController::class, 'destroy'])->name('weekly.destroy');
+    
     // About
     Route::get('/about', [PagesController::class, 'about'])->name('about.index');
 
